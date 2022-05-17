3b00990177fd7e43164dd5d3a27b98ec4a256b07
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-05-16 21:23:04 -0500

    hello world

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "holapapa": "#f0f0f0",
+    },
+  },
+  plugins: [],
+}
